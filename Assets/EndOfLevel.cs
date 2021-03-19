d07435123409619ed5c1e64c1ef3881cee559674
Marcio <marciohp189@gmail.com>
2021-03-19 11:39:49 -0300

    CasteloVR

---
+++ Assets/EndOfLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour
+{
+
+    public void ChangeOfGame()
+    {
+        //Debug.Log("HIT");
+        //Application.Quit();        
+        SceneManager.LoadScene("Sala");
+    }
+    public void EndOfGame()
+    {   //Debug.Log("HIT");
+        Application.Quit();
+        //SceneManager.LoadScene("Sala");
+
+    }
+}
